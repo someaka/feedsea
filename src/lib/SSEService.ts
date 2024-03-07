010c43e5b99a6c7381bc60059f2e1e8571d9153a
someaka <music.aka.music@gmail.com>
2024-03-07 20:25:49 +0100

    🧪

--- src/lib/SSEService.ts
+++ src/lib/SSEService.ts
@@ -1,74 +1,46 @@
 import { articlesStore, selectedFeedsStore } from './stores/stores';
+import { decompress } from './compression';
 import type { ArticleType, FeedChange } from './types';
 
 let eventSource: EventSource | null = null;
-let decompressionWorker: Worker | null = null;
-let idleTimeout: ReturnType<typeof setTimeout>;
-const TIMEOUT_INTERVAL = 10000;
 
-function initializeDecompressionWorker() {
-    import('./decompressionWorker?worker').then(module => {
-        decompressionWorker = new module.default();
-        decompressionWorker.onmessage = (event) => {
-            const articlesBatch: ArticleType[] = event.data;
-            articlesStore.update((currentArticles) => {
-                articlesBatch.forEach(article => {
-                    if (!currentArticles[article.feedId]) currentArticles[article.feedId] = [];
-                    currentArticles[article.feedId].push(article);
-                });
-                return currentArticles;
-            });
-
-            selectedFeedsStore.update(({ feedIds }) => {
-                return {
-                    feedIds: feedIds,
-                    change: {
-                        type: 'new',
-                        feedId: -1,
-                        articles: articlesBatch
-                    } as FeedChange
-                };
-            });
-
-            resetWorkerIdleTimeout();
-        };
-    });
-}
-
-function resetWorkerIdleTimeout() {
-    clearTimeout(idleTimeout);
-    idleTimeout = setTimeout(() => {
-        if (decompressionWorker) {
-            decompressionWorker.terminate();
-            decompressionWorker = null;
-        }
-    }, TIMEOUT_INTERVAL);
-}
-
-function postMessageToDecompressionWorker(data: string) {
-    if (!decompressionWorker) initializeDecompressionWorker();
-    clearTimeout(idleTimeout); // Clear the timeout when a new task starts
-    decompressionWorker?.postMessage(data);
+interface articleFetchedResponse {
+    feedId: string;
+    compressedArticles: string
 }
 
 export function startSSE() {
     if (eventSource) eventSource.close();
     eventSource = new EventSource('/events');
-    initializeDecompressionWorker();
-
     eventSource.addEventListener('articleFetched', (event) => {
-        const { compressedArticles } = JSON.parse(event.data);
-        postMessageToDecompressionWorker(compressedArticles);
+
+        let articlesBatch: ArticleType[] | null = null;
+        const { compressedArticles } = JSON.parse(event.data) as articleFetchedResponse;
+        articlesBatch = decompress(compressedArticles);
+
+        articlesStore.update((currentArticles) => {
+            articlesBatch?.forEach(article => {
+                if (!currentArticles[article.feedId]) currentArticles[article.feedId] = [];
+                currentArticles[article.feedId].push(article);
+            });
+            return currentArticles;
+        });
+
+        selectedFeedsStore.update(({ feedIds }) => {
+            return {
+                feedIds: feedIds,
+                change: {
+                    type: 'new',
+                    feedId: -1,
+                    articles: articlesBatch
+                } as FeedChange
+            };
+        });
+        articlesBatch = null;
     });
 }
 
 export function stopSSE() {
-    if (eventSource) {
-        eventSource.close();
-        eventSource = null;
-    }
-    if (decompressionWorker) {
-        decompressionWorker.terminate();
-        decompressionWorker = null;
-    }
+    if (eventSource) eventSource.close();
+
 }
